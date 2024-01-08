941b74824e84c7107850025ff18456e71b7f984c
isexample <yeji0331@naver.com>
2024-01-09 08:44:03 +0900

    [Feat] 휴게소 식당목록 및 메뉴목록 조회 API

--- src/main/java/hyundai_6th_team/hyundai_6th_team/repository/MenuRepository.java
+++ src/main/java/hyundai_6th_team/hyundai_6th_team/repository/MenuRepository.java
@@ -1,6 +1,7 @@
 package hyundai_6th_team.hyundai_6th_team.repository;
 
 import hyundai_6th_team.hyundai_6th_team.entity.Menu;
+import hyundai_6th_team.hyundai_6th_team.entity.Restaurant;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,4 +9,6 @@ import java.util.List;
 
 public interface MenuRepository extends JpaRepository<Menu, Long> {
     List<Menu> findByRestaurantId(Long restaurantId);
+
+    List<Menu> findByRestaurant(Restaurant restaurant);
 }
