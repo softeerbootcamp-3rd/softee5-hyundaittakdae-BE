5fbf440f5f407c6036d80039472e2f7c68db8d21
isexample <yeji0331@naver.com>
2024-01-08 18:43:37 +0900

    [Feat] Entity 설계 및 단방향 매핑

---
+++ src/main/java/hyundai_6th_team/hyundai_6th_team/entity/RestArea.java
@@ -0,0 +1,33 @@
+package hyundai_6th_team.hyundai_6th_team.entity;
+
+import hyundai_6th_team.hyundai_6th_team.entity.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class RestArea extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "road_id")
+    private Road road;
+
+    @Column(nullable = false)
+    private float posX;
+
+    @Column(nullable = false)
+    private float posY;
+
+    @Column(nullable = false, length = 15)
+    private String phone;
+
+    private String imageUrl;
+
+}
